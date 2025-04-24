980d01d5a16d26e9112f6286af9597578d677c3d
Devin Jean <emailcruzjean@yahoo.com>
2025-04-24 10:44:20 -0500

    stack size test fix

--- src/test/ast.rs
+++ src/test/ast.rs
@@ -2262,5 +2262,5 @@ fn test_stack_size_usage() {
 
     assert_compiles!(src: "projects/stack-size-1.xml", stack_size: 16 * 1024); // this is min stack size on linux
     assert_compiles!(src: "projects/stack-size-2.xml", stack_size: 16 * 1024); // this is min stack size on linux
-    assert_compiles!(src: "projects/stack-size-3.xml", stack_size: 29 * 1024);
+    assert_compiles!(src: "projects/stack-size-3.xml", stack_size: 30 * 1024);
 }
